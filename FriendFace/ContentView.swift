51fb2e7c9b7c6ed7fe2daa2055a5c794a8f772d2
slavabily <slavabily@me.com>
2020-04-22 16:20:36 +0300

    data fetch is done; list with primary info on users done

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -9,9 +9,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var users = [User]()
+        
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(users) {user in
+                        VStack(alignment: .leading) {
+                            Text(user.name)
+                                .fontWeight(.bold)
+                            Text(user.company)
+                        }
+                     }
+                }
+                .onAppear {
+                   self.fetchUsers()
+                }
+            }
+        .navigationBarTitle("Users")
+        }
     }
+    
+    func fetchUsers() {
+             let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                guard let data = data else {
+                    print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+                    return
+                }
+                if let decoded = try? JSONDecoder().decode([User].self, from: data) {
+                    self.users = decoded
+                } else {
+                    print("Invalid response from server")
+                }
+            }.resume()
+     }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ FriendFace/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  FriendFace
+//
+//  Created by slava bily on 21/4/20.
+//  Copyright © 2020 slava bily. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable, Identifiable {
+    
+    struct Friend: Codable {
+        var id: String
+        var name: String
+    }
+    
+    var id: String
+    var name: String
+    var company: String
+    var friends: [Friend]
+    
+}
+
